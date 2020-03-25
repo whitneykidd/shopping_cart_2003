e2e85adfe29c720a7043d18d6ba7a7a685509860
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-25 09:15:06 -0600

    Pass test_can_be_hoarded

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,22 @@
 class Product
-  attr_reader :category, :name, :unit_price, :quantity
+  attr_reader :category, :name, :unit_price, :quantity, :hoarded
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoarded = false
   end
 
   def total_price
     @unit_price * @quantity
+  end
+
+  def is_hoarded?
+    @hoarded
+  end
+
+  def hoard
+    @hoarded = true
   end
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -35,4 +35,12 @@ class ProductTest < Minitest::Test
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
   end
+
+  def test_can_be_hoarded
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+    assert_equal false, product.is_hoarded?
+
+    product.hoard
+    assert_equal true, product.is_hoarded?
+  end
 end
