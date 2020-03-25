588891da2f4fb4876f58de5a70f258fe886d6dc2
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-25 09:05:20 -0600

    Pass test_it_has_a_quantity

--- lib/product.rb
+++ lib/product.rb
@@ -1,10 +1,10 @@
 class Product
-  attr_reader :category, :name, :unit_price
+  attr_reader :category, :name, :unit_price, :quantity
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = quantity
+    @quantity = quantity.to_i
   end
 
   def total_price
--- test/product_test.rb
+++ test/product_test.rb
@@ -24,7 +24,6 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_has_a_quantity
-    skip
     product = Product.new(:paper, 'toilet paper', 3.70, '10')
     assert_equal 10, product.quantity
   end
