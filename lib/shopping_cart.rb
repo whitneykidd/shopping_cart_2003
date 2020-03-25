ad57b280fcd188700eb9f35197fb0fb5598a459b
Whitney Kidd <whitneymkidd@gmail.com>
2020-03-25 09:12:38 -0600

    Pass test_it_can_add_products

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -5,4 +5,8 @@ class ShoppingCart
     @capacity = capacity.to_i
     @products = []
   end
+
+  def add_product(product)
+    @products << product
+  end
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,6 +6,8 @@ require './lib/shopping_cart'
 class ShoppingCartTest < Minitest::Test
   def setup
     @cart = ShoppingCart.new("King Soopers", "30items")
+    @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    @product2 = Product.new(:meat, 'chicken', 4.50, '2')
   end
 
   def test_it_exists
@@ -16,5 +18,11 @@ class ShoppingCartTest < Minitest::Test
     assert_equal 'King Soopers', @cart.name
     assert_equal 30, @cart.capacity
     assert_equal [], @cart.products
+  end
+
+  def test_it_can_add_products
+    @cart.add_product(@product1)
+    @cart.add_product(@product2)
+    assert_equal [@product1, @product2], @cart.products
   end
 end
